1f676cc9e3d8a9af56da666effe31aefb7c2b91c
Generosa Litton <generosa.litton@yahoo.com>
2017-09-12 18:27:41 -0700

    first commit

---
+++ dist/index.js
@@ -0,0 +1,23 @@
+// const { createElement } = React Don't need this
+const { render } = ReactDOM
+// Don't need this
+// const style = { 
+//   backgroundColor: 'pink',
+//   color: 'black',
+//   fontFamily: 'verdana'
+// }
+// Don't need this bec of JSX
+// const title = createElement(
+//   'h1',
+//   {id: 'title', className: 'header', style: style},
+//   'Hello Osay! You are Awesome!'
+// ) 
+
+render(
+  <h1 id='title'      
+      className='header'
+      style={{backgroundColor:'pink', color: 'black', fontFamily: 'verdana'}}>
+  Hello World 
+  </h1>,
+  document.getElementById('react-container')
+)
